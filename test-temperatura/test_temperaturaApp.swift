c0c32e811fcaa9878d7c6f6fbce5e848ad47cf84
Vincent Berihuete <vberihuete@gmail.com>
2024-12-14 20:41:42 -0500

    Initial Commit

---
+++ test-temperatura/test_temperaturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_temperaturaApp.swift
+//  test-temperatura
+//
+//  Created by Vincent Berihuete Paulino on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_temperaturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
